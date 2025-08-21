cab35e893021dd2ef873bf708caaa6b3e3ac0ac7
gagnesh-kakkar <gagneshkakkar4500@gmail.com>
2025-08-21 19:41:18 +0530

    EXP-1.2.java file

--- EXP-1.2/EXP-1.2.java
+++ EXP-1.2/EXP-1.2.java
@@ -21,6 +21,7 @@ class PowerFunction {
     }
 
     public static void main(String[] args) {
+
         System.out.println("2^10 = " + power(2, 10));
         System.out.println("2^-3 = " + power(2, -3));
         System.out.println("5^0 = " + power(5, 0));
