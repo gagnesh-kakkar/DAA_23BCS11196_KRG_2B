cd60d14b48c99350394c74f7ab1d07eda6596d0a
gagnesh-kakkar <gagneshkakkar4500@gmail.com>
2025-08-21 19:33:36 +0530

    EXP-1.2. pdf file

---
+++ EXP-1.2/EXP-1.2.java
@@ -0,0 +1,29 @@
+package DAA;
+
+class PowerFunction {
+
+    static double power(double x, int y) {
+        if (y == 0) {
+            return 1;
+        }
+
+        double temp = power(x, y / 2);
+
+        if (y % 2 == 0) {
+            return temp * temp;
+        } else {
+            if (y > 0) {
+                return x * temp * temp;
+            } else {
+                return (temp * temp) / x;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2^10 = " + power(2, 10));
+        System.out.println("2^-3 = " + power(2, -3));
+        System.out.println("5^0 = " + power(5, 0));
+        System.out.println("3^7 = " + power(3, 7));
+    }
+}
