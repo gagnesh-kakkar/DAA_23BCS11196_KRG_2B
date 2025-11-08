1dc953fad0f8da0aa88582108fc38b534c2e09a0
gagnesh-kakkar <gagneshkakkar4500@gmail.com>
2025-11-08 15:05:08 +0530

    EXP-2.2.pdf & Code.cpp

---
+++ EXP-2.2/Code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool subsetSum(int arr[], int n, int sum) {
+    bool dp[n+1][sum+1];
+    for(int i=0;i<=n;i++) dp[i][0]=true;
+    for(int j=1;j<=sum;j++) dp[0][j]=false;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+            if(arr[i-1] > j)
+                dp[i][j] = dp[i-1][j];
+            else
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+        }
+    }
+    return dp[n][sum];
+}
+
+int main() {
+    int n, sum;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i=0;i<n;i++) cin >> arr[i];
+    cout << "Enter target sum: ";
+    cin >> sum;
+
+    if(subsetSum(arr, n, sum))
+        cout << "Subset with given sum exists";
+    else
+        cout << "Subset with given sum does not exist";
+}
